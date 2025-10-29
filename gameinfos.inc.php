f2e1f88143e42af5523f527a2d303d82edaffb30
Remi Bertin <remiber@free.fr>
2025-10-29 12:17:52 +0100

    Remove deprecated keys in gameinfos

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -15,15 +15,6 @@ $gameinfos = array(
 // Name of the game in English (will serve as the basis for translation)
 'game_name' => "Not Alone",
 
-// Game designer (or game designers, separated by commas)
-'designer' => 'Ghislain Masson',
-
-// Game artist (or game artists, separated by commas)
-'artist' => 'Sébastien Caiveau',
-
-// Year of FIRST publication of this game. Can be negative.
-'year' => 2016,
-
 // Game publisher
 'publisher' => 'Geek Attitude Games',
 
@@ -44,9 +35,8 @@ $gameinfos = array(
 'suggest_player_number' => 5,
 
 // Discourage players to play with these numbers of players. Must be null if there is no such advice.
-'not_recommend_player_number' => 2,
 // 'not_recommend_player_number' => array( 2, 3 ),      // <= example: this is not recommended to play this game with 2 or 3 players
-
+'not_recommend_player_number' => array( 2 ),
 
 // Estimated game duration, in minutes (used only for the launch, afterward the real duration is computed)
 'estimated_duration' => 30,
@@ -72,24 +62,8 @@ $gameinfos = array(
 // The game end result will display "Winner" for the 1st player and "Loser" for all other players
 'losers_not_ranked' => true,
 
-// Game is "beta". A game MUST set is_beta=1 when published on BGA for the first time, and must remains like this until all bugs are fixed.
-'is_beta' => 1,
-
 // Is this game cooperative (all players wins together or loose together)
 'is_coop' => 0,
-
-
-// Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
-'complexity' => 3,
-
-// Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
-'luck' => 3,
-
-// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 3,
-
-// Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
-'diplomacy' => 3,
 
 // Colors attributed to players
 'player_colors' => array( "ff0000", "008000", "0000ff", "ffa500", "773300", "000000", "800080" ),
@@ -114,23 +88,4 @@ $gameinfos = array(
     'max' => null
 ),
 
-// Games categories
-//  You can attribute a maximum of FIVE "tags" for your game.
-//  Each tag has a specific ID (ex: 22 for the category "Prototype", 101 for the tag "Science-fiction theme game")
-//  Please see the "Game meta information" entry in the BGA Studio documentation for a full list of available tags:
-//  http://en.doc.boardgamearena.com/Game_meta-information:_gameinfos.inc.php
-//  IMPORTANT: this list should be ORDERED, with the most important tag first.
-//  IMPORTANT: it is mandatory that the FIRST tag is 1, 2, 3 and 4 (= game category)
-'tags' => array( 3,11,101,205 ),
-
-
-//////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
-
-// simple : A plays, B plays, C plays, A plays, B plays, ...
-// circuit : A plays and choose the next player C, C plays and choose the next player D, ...
-// complex : A+B+C plays and says that the next player is A+B
-'is_sandbox' => false,
-'turnControl' => 'simple'
-
-////////
 );
